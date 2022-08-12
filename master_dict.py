260872cd82ccf0b71d15824bef050fd967eccc91
Greg <gregh13@protonmail.com>
2022-08-12 14:20:52 +0700

    add all api calls, merge all dictionaries into one

--- master_dict.py
+++ master_dict.py
@@ -11,7 +11,6 @@ if not os.environ.get("API_KEY"):
 def stock_check(symbol):
     """Gets current stock data for symbol."""
 
-    # Contact API
     try:
         api_key = os.environ.get("API_KEY")
         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote/marketCap?token={api_key}"
@@ -26,8 +25,6 @@ def stock_check(symbol):
         print(f"\n\nQUOTE:\n\n{quote}\n\n")
         return {
             symbol: quote["marketCap"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
         }
     except (KeyError, TypeError, ValueError):
         try:
@@ -36,13 +33,68 @@ def stock_check(symbol):
         except:
             return None
 
-market_caps = {}
 
+def billionaire_check():
+    """Gets current net worth for the richest billionaires."""
+    dict = {}
+    url = "https://forbes400.herokuapp.com/api/forbes400?limit=10"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        results = response.json()
+    except requests.RequestException:
+        return None
+    print("\nBillionaire Data Processing:")
+    for result in results:
+        wealth = result["finalWorth"]
+        fortune = float(wealth) * 1000000
+        dict[result["personName"]] = fortune
+    print(dict)
+    return dict
+
+
+def crypto_check():
+    """Gets current financial data for top cryptocurrencies."""
+    dict = {}
+    url = "https://api.coinlore.net/api/tickers/?start=0&limit=5"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        results = response.json()
+    except requests.RequestException:
+        return None
+    print("\n Cryptocurrency Data Processing:")
+    for result in results["data"]:
+        cap = result["market_cap_usd"]
+        total = round(float(cap))
+        dict[results["name"]] = total
+    print(dict)
+    return dict
+
+master_dictionary = {}
+stock_caps = {}
+billionaires = {}
+cryptos = {}
+
+# Look up current stock price to get accurate marketcap valuations
 for symbol in stocks:
     m_cap = stock_check(symbol)
     print(f"\nreturned value:\n{m_cap}\n")
     # creates new dictionary with {company name: market cap}
-    market_caps[stocks["symbol"]] = m_cap[symbol]
+    stock_caps[stocks["symbol"]] = m_cap[symbol]
+print(stock_caps)
 
-print(market_caps)
+# Look up current net worth of top 10 billionaires
+billionaires = billionaire_check()
+
+# Look up current market cap for top 5 cryptocurrencies
+cryptos = crypto_check()
+
+master_dictionary = countries | man_made | stock_caps | billionaires | cryptos
+
+print("\nMASTER DICTIONARY\n")
+print(master_dictionary)
+print("\n")
+
+
 
