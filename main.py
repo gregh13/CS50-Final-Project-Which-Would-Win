204935bfe59571ebe168486230ab93cee9401fbb
Greg <gregh13@protonmail.com>
2022-08-13 13:17:29 +0700

    configure storing new data into db and calling that data in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from master_dict import master_dictionary
+from mm_dicts import countries, man_made
 app = Flask(__name__)
 
 # Configure app and sessions
@@ -31,32 +31,42 @@ class Highscores(db.Model):
     timestamp = db.Column(db.String(100), nullable=False)
 
 
+class Marketdata(db.Model):
+    __tablename = "marketdata"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.String(100), nullable=False)
+    image_src = db.Column(db.Text)
+
+
 db.create_all()
 
-# master_dictionary = {
-#     'Japan': 4912150000000, 'Germany': 4256540000000, 'United Kingdom': 3376000000000, 'India': 3534740000000,
-#     'France': 2936700000000, 'Italy': 2058330000000, 'Brazil': 1833270000000, 'Canada': 2221220000000,
-#     'Russia': 1829050000000, 'Korea': 1804680000000, 'Ireland': 516146000000, 'Greece': 222770000000,
-#     'Kenya': 114679000000, 'Croatia': 69459000000, 'Nepal': 36315000000, 'Cambodia': 28020000000,
-#     'Iceland': 27865000000, 'Jamaica': 15721000000, 'Mongolia': 18102000000, 'Maldives': 5502000000,
-#     'US Interstate Highway System': 535000000000, 'Great Wall of China (Estimate)': 260000000000,
-#     'International Space Station': 160000000000, '50 B-2 Stealth Bombers': 105000000000,
-#     '5 of the Most Expensive Aircraft Carrier': 65000000000, '200,000 Lambos': 60000000000,
-#     '30 Burj Khalifas (Tallest Building)': 45000000000, 'Most Expensive Airport (Osaka, Japan)': 29000000000,
-#     'ALL of the Royal Caribbean Cruise Ships': 25000000000, 'The Channel Tunnel (UK<-->FR)': 22400000000,
-#     "Largest Diamond Mine (By Reserve)": 604000000000, "1 Trillion Slices of Kraft Singles Cheese": 208300000000,
-#     "ALL of United Airlines Airplanes": 108000000000, "100 Million Years of Netflix Subscription": 37176000000,
-#     'Apple': 2707761004480, 'Microsoft': 2140564125101, 'Google': 80898990179, 'Amazon': 1432777709604,
-#     'Tesla': 898146518998, 'Meta Platforms': 506556482719, 'Visa': 465220320000, 'Exxon Mobil': 388382026331,
-#     'Coca-Cola': 273403056380, 'McDonald': 190756774025, 'AT&T': 128553040000, 'Netflix': 107223648442,
-#     'Starbucks': 100133598000, 'Target': 78642911645, 'Airbnb': 73197432000, 'Ford': 62518771547, 'Dell': 34462400000,
-#     'Zoom': 30905917284, "Kellog's": 25680000000, "American Airlines": 9740000000, 'Elon Musk': 255940425000,
-#     'Bernard Arnault & family': 174656024000, 'Jeff Bezos': 165114752000,
-#     'Gautam Adani & family': 130849543000, 'Bill Gates': 113133936000, 'Larry Ellison': 106393052000,
-#     'Warren Buffett': 102948495000, 'Larry Page': 101384351000, 'Sergey Brin': 98284195000,
-#     'Mukesh Ambani': 95705176000, 'Bitcoin': 457507207036, 'Ethereum': 229595181591, 'Tether': 66110192252,
-#     'USD Coin': 54252182355, 'Binance Coin': 53809039092
-# }
+# Debugging mode, temporarily allow hard-code dictionary
+master_dictionary = {
+    'Japan': 4912150000000, 'Germany': 4256540000000, 'United Kingdom': 3376000000000, 'India': 3534740000000,
+    'France': 2936700000000, 'Italy': 2058330000000, 'Brazil': 1833270000000, 'Canada': 2221220000000,
+    'Russia': 1829050000000, 'Korea': 1804680000000, 'Ireland': 516146000000, 'Greece': 222770000000,
+    'Kenya': 114679000000, 'Croatia': 69459000000, 'Nepal': 36315000000, 'Cambodia': 28020000000,
+    'Iceland': 27865000000, 'Jamaica': 15721000000, 'Mongolia': 18102000000, 'Maldives': 5502000000,
+    'US Interstate Highway System': 535000000000, 'Great Wall of China (Estimate)': 260000000000,
+    'International Space Station': 160000000000, '50 B-2 Stealth Bombers': 105000000000,
+    '5 of the Most Expensive Aircraft Carrier': 65000000000, '200,000 Lambos': 60000000000,
+    '30 Burj Khalifas (Tallest Building)': 45000000000, 'Most Expensive Airport (Osaka, Japan)': 29000000000,
+    'ALL of the Royal Caribbean Cruise Ships': 25000000000, 'The Channel Tunnel (UK<-->FR)': 22400000000,
+    "Largest Diamond Mine (By Reserve)": 604000000000, "1 Trillion Slices of Kraft Singles Cheese": 208300000000,
+    "ALL of United Airlines Airplanes": 108000000000, "100 Million Years of Netflix Subscription": 37176000000,
+    'Apple': 2707761004480, 'Microsoft': 2140564125101, 'Google': 80898990179, 'Amazon': 1432777709604,
+    'Tesla': 898146518998, 'Meta Platforms': 506556482719, 'Visa': 465220320000, 'Exxon Mobil': 388382026331,
+    'Coca-Cola': 273403056380, 'McDonald': 190756774025, 'AT&T': 128553040000, 'Netflix': 107223648442,
+    'Starbucks': 100133598000, 'Target': 78642911645, 'Airbnb': 73197432000, 'Ford': 62518771547, 'Dell': 34462400000,
+    'Zoom': 30905917284, "Kellog's": 25680000000, "American Airlines": 9740000000, 'Elon Musk': 255940425000,
+    'Bernard Arnault & family': 174656024000, 'Jeff Bezos': 165114752000,
+    'Gautam Adani & family': 130849543000, 'Bill Gates': 113133936000, 'Larry Ellison': 106393052000,
+    'Warren Buffett': 102948495000, 'Larry Page': 101384351000, 'Sergey Brin': 98284195000,
+    'Mukesh Ambani': 95705176000, 'Bitcoin': 457507207036, 'Ethereum': 229595181591, 'Tether': 66110192252,
+    'USD Coin': 54252182355, 'Binance Coin': 53809039092
+}
 
 LIST_LENGTH = len(master_dictionary)
 # Since questions require two choices, if list length is odd, take off one for more accurate counter check later on
@@ -70,13 +80,13 @@ def index():
     session.clear()
 
     if request.method == "POST":
-        # Test heroku run/update process
-        print("\nSTART GAME Dictionary:\n")
-        print(master_dictionary)
-        print("\n")
+        # Grab current data from db
+        all_rows = db.session.query(Marketdata)
+        for row in all_rows:
+            print(row)
 
-        # Clear out any previous store values
-        session.clear()
+        # Test heroku run/update process
+
 
         # Start Game
         order = []
--- master_dict.py
+++ master_dict.py
@@ -1,7 +1,9 @@
 import os
 import requests
 import urllib.parse
-from mm_dicts import countries, stocks, man_made
+import datetime
+from mm_dicts import stocks
+from main import Marketdata, db
 import random
 
 # Make sure API key is set
@@ -9,20 +11,32 @@ if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
 API_KEY = os.environ.get("API_KEY")
-
+timestamp = datetime.datetime.now()
 random_num = random.randint(1, 2222)
+
+
+def google_check(symbol):
+    """Manually calculates market cap for Google since normal IEX Api route returns oddly low valuations"""
+    try:
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={API_KEY}"
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        price = float(quote["latestPrice"])
+        # Google has roughly 13 billion shares
+        market_cap = int(price * 13044000000)
+        return {symbol: market_cap}
+    except (KeyError, TypeError, ValueError):
+        return None
 
 
 def stock_check(symbol):
     """Gets current stock data for symbol."""
-    if symbol == "GOOG":
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={API_KEY}"
-        response = requests.get(url)
-        response.raise_for_status()
-        debug = response.json()
-        print("\nGOOGLE QUOTE:\n")
-        print(debug)
-        print("\n\n")
     try:
         url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote/marketCap?token={API_KEY}"
         response = requests.get(url)
@@ -45,16 +59,18 @@ def billionaire_check():
     try:
         response = requests.get(url)
         response.raise_for_status()
-        results = response.json()
     except requests.RequestException:
         return None
-    print("\nBillionaire Data Processing:")
-    for result in results:
-        wealth = result["finalWorth"]
-        fortune = int(float(wealth) * 1000000)
-        dict[result["personName"]] = fortune
-    print(dict)
-    return dict
+    # Parse response
+    try:
+        results = response.json()
+        for result in results:
+            wealth = result["finalWorth"]
+            fortune = int(float(wealth) * 1000000)
+            dict[result["personName"]] = fortune
+        return dict
+    except (KeyError, TypeError, ValueError):
+        return None
 
 
 def crypto_check():
@@ -64,30 +80,34 @@ def crypto_check():
     try:
         response = requests.get(url)
         response.raise_for_status()
-        results = response.json()
     except requests.RequestException:
         return None
-    print("\n Cryptocurrency Data Processing:")
-    for result in results["data"]:
-        cap = result["market_cap_usd"]
-        total = round(float(cap))
-        dict[result["name"]] = total
-    print(dict)
-    return dict
+    # Parse response
+    try:
+        results = response.json()
+        for result in results["data"]:
+            cap = result["market_cap_usd"]
+            total = round(float(cap))
+            dict[result["name"]] = total
+        return dict
+    except (KeyError, TypeError, ValueError):
+        return None
 
 
-master_dictionary = {}
+payload = {}
 stock_caps = {}
 billionaires = {}
 cryptos = {}
 
-print(stocks)
 # Look up current stock price to get accurate marketcap valuations
 for symbol in stocks:
-    m_cap = stock_check(symbol)
+    # IEX Stock data for market caps returns odd values for Google
+    if symbol == "GOOG":
+        m_cap = google_check(symbol)
+    else:
+        m_cap = stock_check(symbol)
     # creates new dictionary with {company name: market cap}
     stock_caps[stocks[symbol]] = m_cap[symbol]
-print(stock_caps)
 
 # Look up current net worth of top 10 billionaires
 billionaires = billionaire_check()
@@ -95,14 +115,34 @@ billionaires = billionaire_check()
 # Look up current market cap for top 5 cryptocurrencies
 cryptos = crypto_check()
 
-master_dictionary = countries | man_made | stock_caps | billionaires | cryptos
+payload = stock_caps | billionaires | cryptos
 
-# Testing for heroku run/update process
-master_dictionary["random_number"] = random_num
-print(f"Random num: {random_num}")
-print(master_dictionary["random_number"])
-print("INNER dictionary")
-print(master_dictionary)
+# Test whether main.py receives updated table info
+payload["random_num"] = random_num
+
+# Clear table values
+try:
+    num_rows_deleted = db.session.query(Marketdata).delete()
+    db.session.commit()
+except:
+    db.session.rollback()
+
+# Create table objects from payload to batch update Marketdata
+objects = []
+for key in payload:
+    objects.append(Marketdata(name=key, value=payload[key], timestamp=timestamp))
+
+db.bulk_save_objects(objects)
+db.commit()
+
+
+# Batch update without deleting rows option
+# Problems arise if a new entry comes along (new billionaire/crypto)
+# db.query(Marketdata).filter(Marketdata.name.in_(payload)).update(
+#     {Marketdata.value: case(payload, value=Marketdata.col1,)})
+
+
+
 
 
 
